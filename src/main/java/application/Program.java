ef4d7c1be8fe834414099a5a4030264a253c939e
Daniel <daniielsouzapvh@gmail.com>
2026-02-13 00:18:16 -0300

    Seller entity class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     static void main(String[] args) {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+        Seller seller = new Seller(1,"daniel", "daniel@gmail.com", LocalDate.now(), 3000.00, obj);
+        System.out.println("seller "+ seller);
     }
 }
